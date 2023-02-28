d38bc2926387a665ac662aeaea7afb5c08c4cc7f
VAdimT <vadimt382@gmail.com>
2023-02-28 19:45:40 +0300

    3l

--- src/main/java/ru/tolstov/Main.java
+++ src/main/java/ru/tolstov/Main.java
@@ -2,6 +2,20 @@ package ru.tolstov;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int a = 20;
+        int b = 35;
+
+        //  Арифметические операторы
+        System.out.println("a + b = " + (a + b));
+        System.out.println("a - b = " + (a - b));
+        System.out.println("a * b = " + (a * b));
+        System.out.println("a / b = " + (a / b));
+        System.out.println("double a / b = " + (double) a / b);
+        System.out.println("a % b = " + ( b % a));
+        System.out.println("Инкремент " + ++a);
+        System.out.println("Декремент  " + --b);
+
+
     }
 }
